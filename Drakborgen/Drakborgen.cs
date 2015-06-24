6f13af72f68bd83be1245e9c51aad9a671e5d9b6
Henrik Aronsson <arnssn@gmail.com>
2015-06-24 20:36:06 +0200

    First commit

---
+++ Drakborgen/Drakborgen.cs
@@ -0,0 +1,67 @@
+﻿using Drakborgen.States;
+using Gengine;
+using Microsoft.Xna.Framework;
+
+namespace Drakborgen {
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Drakborgen : Game {
+        GraphicsDeviceManager _graphics;
+        private HGengine _engine;
+
+        public Drakborgen() {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize() {
+            _engine = new HGengine(this, _graphics);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent(){
+            _engine.Start(640, 360, 1280, 720);
+            _engine.AddState("mainmenu", new MainMenu());
+            _engine.AddFont("text", "Fonts/04b_03");
+            _engine.AddTexture("tiles32.png", "Sprites/tiles32");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent(){
+            _engine.Unload();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime){
+            _engine.Update((float) gameTime.ElapsedGameTime.TotalSeconds);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime) {
+            _engine.Draw();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Drakborgen/States/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using Gengine.State;
+using Gengine.UI;
+using Microsoft.Xna.Framework;
+
+namespace Drakborgen.States {
+    public class MainMenu : BaseMenuState {
+        public override void Init() {
+            SetAction(HandleCommand);
+            SetTitle(new MenuOption("text", "Drakborgen", Color.Red, new Vector2(World.View.Center.X - 50, World.View.Center.Y - 50)));
+            AddOption(new MenuOption("text", "Exit", Color.White, new Vector2(World.View.Center.X - 40, World.View.Center.Y + 40)));
+        }
+
+        private void HandleCommand(string command) {
+            if (command == "Escape") {
+                StateManager.ClearStates();
+            }
+            if (command == "Enter") {
+                if (SelectedOption == "Exit") {
+                    StateManager.ClearStates();
+                    //StateManager.PushState("game");
+                }
+            }
+        }
+    }
+}
